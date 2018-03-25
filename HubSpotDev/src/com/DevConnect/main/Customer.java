d7d56673a9a7397a95c15a62e45f4e90e8822d4e
kankishore@gmail.com <kankishore@gmail.com>
2018-03-25 12:57:50 +0530

    First Commit

---
+++ HubSpotDev/src/com/DevConnect/main/Customer.java
@@ -0,0 +1,22 @@
+package com.DevConnect.main;
+
+public class Customer {
+
+	
+	
+	public int addCustomer(String[] ArrayList){
+		
+		int cId=0;
+		
+		//validate values from UI
+		
+		//Insert in the DB
+		return cId;
+		
+		
+		
+	}
+	
+	
+	
+}
